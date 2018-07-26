87d1458f036edd2f058dacef52bfe8d127f0de83
IslamTareq0012 <31237314+IslamTareq0012@users.noreply.github.com>
2018-07-26 23:59:07 +0200

    home page style finished

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,5 +10,28 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+  showSocialIcons() {
+    var gridDiv = document.querySelector(".social_icons");
+    console.log("grid socials style", gridDiv.style);
 
+    if (gridDiv.style.maxHeight) {
+      gridDiv.style.maxHeight = null;
+    } else {
+      gridDiv.style.maxHeight = gridDiv.scrollHeight + "px";
+    }
+    //var i;
+
+    // for (i = 0; i < coll.length; i++) {
+    //   coll[i].addEventListener("click", function () {
+    //     this.classList.toggle("active");
+    //     var content = this.nextElementSibling;
+    //     if (content.style.maxHeight) {
+    //       content.style.maxHeight = null;
+    //     } else {
+    //       content.style.maxHeight = content.scrollHeight + "px";
+    //     }
+    //   });
+    // }
+  }
 }
+
