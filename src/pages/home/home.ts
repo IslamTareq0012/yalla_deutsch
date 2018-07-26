5d7bab1c51fecb2ea444121b1881e3a2a7970ec1
IslamTareq0012 <31237314+IslamTareq0012@users.noreply.github.com>
2018-07-27 00:05:44 +0200

    dummy comments removed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -19,19 +19,6 @@ export class HomePage {
     } else {
       gridDiv.style.maxHeight = gridDiv.scrollHeight + "px";
     }
-    //var i;
-
-    // for (i = 0; i < coll.length; i++) {
-    //   coll[i].addEventListener("click", function () {
-    //     this.classList.toggle("active");
-    //     var content = this.nextElementSibling;
-    //     if (content.style.maxHeight) {
-    //       content.style.maxHeight = null;
-    //     } else {
-    //       content.style.maxHeight = content.scrollHeight + "px";
-    //     }
-    //   });
-    // }
   }
 }
 
