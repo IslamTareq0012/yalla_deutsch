288a670e96896af188824faa86cecefaf0a6c605
IslamTareq0012 <31237314+IslamTareq0012@users.noreply.github.com>
2018-07-28 13:34:10 +0200

    firebase added to events page module

--- src/pages/events-list/events-list.module.ts
+++ src/pages/events-list/events-list.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { EventsListPage } from './events-list';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 @NgModule({
   declarations: [
@@ -8,6 +10,15 @@ import { EventsListPage } from './events-list';
   ],
   imports: [
     IonicPageModule.forChild(EventsListPage),
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyBjFwdUFZuRuf8IAvEAOO5QUGOQnwc-J9Y",
+      authDomain: "yalla-ddeutsch.firebaseapp.com",
+      databaseURL: "https://yalla-ddeutsch.firebaseio.com",
+      projectId: "yalla-ddeutsch",
+      storageBucket: "yalla-ddeutsch.appspot.com",
+      messagingSenderId: "932234677411"
+    })
   ],
 })
-export class EventsListPageModule {}
+export class EventsListPageModule { }
